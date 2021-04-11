7c996dde1828daa1810560b8391bd58395c0dc89
jeebster <freedomtoroam@posteo.de>
2021-04-10 20:04:59 -0500

    concurrently fetch data

--- main.go
+++ main.go
@@ -7,13 +7,34 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
+	"sync"
 
 	"github.com/jeebster/cryptofolio/asset"
 	"golang.org/x/text/message"
 )
 
 const MANIFEST_FILENAME = "cryptocurrencies.json"
+
+func fetchDataWorker(wg *sync.WaitGroup, c *asset.Cryptocurrency, printer *message.Printer) {
+	defer wg.Done()
+
+	err := c.FetchData()
+	if err != nil {
+		log.Fatal(
+			printer.Printf("Error fetching data from Coincap.io: %s", err),
+		)
+	}
+
+	printer.Printf(
+		"%f %s at $%f USD = $%f\n",
+		c.Quantity,
+		strings.ToUpper(
+			c.Symbol,
+		),
+		c.Price,
+		c.Balance,
+	)
+}
 
 func parseManifestToCryptocurrencyAssets(filepath string) ([]asset.Cryptocurrency, error) {
 	var assetData []asset.Cryptocurrency
@@ -56,32 +77,20 @@ func main() {
 		)
 	}
 
-	for i, c := range cryptocurrencies {
+	var wg sync.WaitGroup
+
+	printer.Printf("Fetching price data from Coincap.io...\n\n")
+
+	for i, _ := range cryptocurrencies {
 		// second element returned from range is a *COPY* of underlying array element
 		// https://tour.golang.org/moretypes/7
 
-		printer.Printf("Fetching price data for %s from Coincap.io...\n", c.Id)
-
 		elementRef := &cryptocurrencies[i]
-		err := elementRef.FetchData()
-		if err != nil {
-			log.Fatal(
-				printer.Printf("Error fetching data from Coincap.io: %s", err),
-			)
-		}
 
-		printer.Printf(
-			"%f %s at $%f USD = $%f\n\n",
-			c.Quantity,
-			strings.ToUpper(
-				elementRef.Symbol,
-			),
-			elementRef.Price,
-			elementRef.Balance,
-		)
-
-		time.Sleep(1 * time.Second)
+		wg.Add(1)
+		go fetchDataWorker(&wg, elementRef, printer)
 	}
 
-	printer.Printf("Total portfolio balance: $%f", asset.CalculateTotalAssetsBalance(cryptocurrencies))
+	wg.Wait()
+	printer.Printf("\nTotal portfolio balance: $%f", asset.CalculateTotalAssetsBalance(cryptocurrencies))
 }
